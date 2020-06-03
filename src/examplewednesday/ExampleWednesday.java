717428340a82d879fd91cdb611fb1cc0772a2c08
Josh-S-23 <sajoshua@sheridan.desire2learn.com>
2020-06-03 02:32:59 -0400

    First Commit

---
+++ src/examplewednesday/ExampleWednesday.java
@@ -0,0 +1,18 @@
+
+
+package examplewednesday;
+
+/**
+ *
+ * @author lil_b
+ */
+public class ExampleWednesday {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("Learning Push");
+    }
+
+}
